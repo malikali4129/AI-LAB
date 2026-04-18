039a3a5554eaa156ce35b0e1d5f22bc644a67bac
Ali Imran Malik <malikali4129@gmail.com>
2026-04-19 01:36:45 +0500

    Revert "Update script.js"
    This reverts commit 5ffab413f175a080e6bd388250be61e17191bab7.

--- script.js
+++ script.js
@@ -111,13 +111,6 @@ const MODULE_REGISTRY = [
     menuTitle: "About Me",
     homeTitle: "About Me",
     homeDescription: "Sample bio section with a functional contact form"
-  },
-  {
-    href: "neonmaze.pages.dev",
-    icon: "🫦",
-    shortTitle: "lip short",
-    menuTitle: "lip meny",
-    homeDescription: "testtttttttttttttttttttttttttttttttttttttttttttttttttt"
   }
 ];
 
